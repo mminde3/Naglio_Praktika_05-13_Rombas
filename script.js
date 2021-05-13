0a040db314873e7a3c3e196ec8ffe13690ebb812
Mindaugas <mminde3@gmail.com>
2021-05-14 01:39:55 +0300

    Rombas
    Su splavom

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 
-
 let rombas= ``;
-
 star = "";
 
 for (let i = 0; i < 21; i++) {
-    star += '*';
+    let number = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
+    star += `<spam style="color: #${number};">*</spam>`;
     rombas += `<div class="line">${star}</div>`;
-    //color.innerHTML = "#" + randomColor;
+
 }
 
 for (var i = 21; i > 0; i--) {
-    star = star.substring(1);
-    rombas += `<div class="line">${star}</div>`;
+    star = star.slice(0, -38);
+   rombas += `<div class="line">${star}</div>`;
 }
 
 document.getElementById("rombas").innerHTML = rombas;
 
-
+// Unmute code for special effects
+// setTimeout(function(){
+//     location.reload();
+// }, 500)
