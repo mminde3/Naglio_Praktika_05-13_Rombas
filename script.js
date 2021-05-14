019ec4a4aff0debb68e3db822158bf270a1795cf
Mindaugas <mminde3@gmail.com>
2021-05-14 10:22:58 +0300

    Perdariau dabar spalvos skiriasi
    Perdariau koda kad visu zvaigzduciu spalvos butu atsitiktines.

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 
 let rombas= ``;
 star = "";
-
+//Didejanti iki 21
 for (let i = 0; i < 21; i++) {
-    let number = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
-    star += `<spam style="color: #${number};">*</spam>`;
+    star = "";
+    x = i;
+    for (x = i; x > 0; x--) {
+        let number = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
+        star += `<spam style="color: #${number};">*</spam>`;
+    }
     rombas += `<div class="line">${star}</div>`;
-
 }
-
-for (var i = 21; i > 0; i--) {
-    star = star.slice(0, -38);
-   rombas += `<div class="line">${star}</div>`;
+//Mazejanti iki 21
+for (let i = 0; i < 21; i++) {
+    star = "";
+    x = i;
+    for (x = i; x < 21; x++) {
+        let number = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
+        star += `<spam style="color: #${number};">*</spam>`;
+    }
+    rombas += `<div class="line">${star}</div>`;
 }
 
 document.getElementById("rombas").innerHTML = rombas;
 
-// Unmute code for special effects
-// setTimeout(function(){
-//     location.reload();
-// }, 500)
